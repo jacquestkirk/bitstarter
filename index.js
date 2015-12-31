cb7a2ab3c078f0f16119226cff22d3f2992eed46
jacquestkirk <jacquestkirk@gmail.com>
2015-12-30 20:51:07 -0600

    add index.html, call it from index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var express = require('express')
 var app = express()
+var fs = require('fs')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!')
+app.get('/', function (request, response) {
+    var index_html = fs.readFileSync('index.html')
+    response.send(index_html.toString())
 })
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+    console.log("Node app is running at localhost:" + app.get('port'))
 })
